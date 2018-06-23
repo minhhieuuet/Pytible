e19bd1c208943b5a079c551c2ff8deb5aa866ded
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2018-06-23 12:13:55 +0700

    Update text when pair

--- Chatible.py
+++ Chatible.py
@@ -17,16 +17,16 @@ def handleUser(senderId,timestamp,message,name,profile_pic,gender):
         print(result)
         if result is None:
             usersCollection.insert_one({ "_id": int(senderId), "idCouple":None, "status": 1, "favorite":'any' , "timestamp": timestamp, "name": name, "gender": gender, "profile_pic": profile_pic})
-            handleMessage(senderId,"Đang tìm bạn","text")
+            handleMessage(senderId,"Chờ 1 chút nhaaa :D Đang tìm bạn cho bạn nèee","text")
             startSession()
         else:
             status = result['status']
             if(status==0):
                 usersCollection.update_one({"_id":int(senderId)},{"$set":{"status":1,"timestamp":timestamp}})
-                handleMessage(senderId,"Đang tìm bạn","text")
+                handleMessage(senderId,"Đang tìm bạn cho bạn nèee","text")
                 startSession()
             if(status==1):
-                handleMessage(senderId,"wait","text")
+                handleMessage(senderId,"Bình tĩnh nào D:","text")
             if(status==2):
                 partnerId = result['idCouple']
                 if(isFacebookMessage(message)):
@@ -52,8 +52,8 @@ def startSession():
             user2 = result[random.randint(0,len(result)-1)]["_id"]
         usersCollection.update_one({"_id":int(user1)},{"$set":{"status":2,"timestamp":None,"idCouple":user2}})
         usersCollection.update_one({"_id":int(user2)},{"$set":{"status":2,"timestamp":None,"idCouple":user1}})
-        handleMessage(user1,"Tìm thấy bạn","text")
-        handleMessage(user2,"Tìm thấy bạn","text")
+        handleMessage(user1,"Hì tìm thấy bạn rùi thử chào nhau 1 câu xem nàoo","text")
+        handleMessage(user2,"Hì tìm thấy bạn rùi thử chào nhau 1 câu xem nàoo","text")
 
 def baibai(senderId,partnerId):
     usersCollection = app.mongo.db.Users
