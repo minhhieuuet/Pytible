c458c33e17c9396a2a3faab95d043b3b0f279e09
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2018-06-24 21:16:09 +0700

    Added json to post

--- refresh.py
+++ refresh.py
@@ -1,4 +1,4 @@
 import requests
 import os
-r = requests.delete('https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+os.getenv("FACEBOOK_TOKEN"))
+r = requests.delete('https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+os.getenv("FACEBOOK_TOKEN"),json={"fields":["persistent_menu"]})
 print (r.status_code)
