6edeb9333b39f5e2ea4850d05c18c44245fdcb1d
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2018-06-24 10:54:19 +0700

    Cronjob with heroku scheduler delete menu

--- refresh.py
+++ refresh.py
@@ -1,1 +1,4 @@
-print("Hello")
+import requests
+import os
+r = requests.delete('https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+os.getenv("FACEBOOK_TOKEN"))
+return str(r.status_code)
