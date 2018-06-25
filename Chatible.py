4b465431372721196c74d16e6d960637f98d2ad0
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2018-06-25 08:35:36 +0700

    Added #windao

--- Chatible.py
+++ Chatible.py
@@ -10,32 +10,42 @@ def isFacebookMessage(url):
     if ('fbcdn.net' in o.hostname) and (o.path.endswith('.png') or o.path.endswith('.jpg') or o.path.endswith('.jpeg') or o.path.endswith('.gif')):
         return url.split(" ")
     return False
+def isCommand(msg):
+    if(msg.startswith("//")):
+        return msg.split("//")[1]
+    return False
 
 def handleUser(senderId,timestamp,message,name,profile_pic,gender):
         usersCollection = app.mongo.db.Users
         result = usersCollection.find_one({'_id':int(senderId)})
         if result is None:
             usersCollection.insert_one({ "_id": int(senderId), "idCouple":None, "status": 1, "favorite":'any' , "timestamp": timestamp, "name": name, "gender": gender, "profile_pic": profile_pic})
-            handleMessage(senderId,"Chờ 1 chút nhaaa :D Đang tìm bạn cho bạn nèee","text")
-            startSession()
+            if(isCommand(message)):
+                code(senderId,isCommand(message).lower(),1)
+            else:
+                handleMessage(senderId,"Chờ 1 chút nhaaa :D Đang tìm bạn cho bạn nèee","text")
+                startSession()
         else:
             status = result['status']
-            if(status==0):
-                usersCollection.update_one({"_id":int(senderId)},{"$set":{"status":1,"timestamp":timestamp}})
-                handleMessage(senderId,"Đang tìm bạn cho bạn nèee","text")
-                startSession()
-            if(status==1):
-                handleMessage(senderId,"Bình tĩnh nào D:","text")
-            if(status==2):
-                partnerId = result['idCouple']
-                isImage = isFacebookMessage(message)
-                if(isImage):
-                    handleMessage(partnerId,isImage,"image")
-                else:
-                    if(message.lower()=="pp"):
-                        baibai(senderId,partnerId)
+            if(isCommand(message)):
+                code(senderId,isCommand(message).lower(),status)
+            else:
+                if(status==0):
+                    usersCollection.update_one({"_id":int(senderId)},{"$set":{"status":1,"timestamp":timestamp}})
+                    handleMessage(senderId,"Đang tìm bạn cho bạn nèee","text")
+                    startSession()
+                if(status==1):
+                    handleMessage(senderId,"Bình tĩnh nào D:","text")
+                if(status==2):
+                    partnerId = result['idCouple']
+                    isImage = isFacebookMessage(message)
+                    if(isImage):
+                        handleMessage(partnerId,isImage,"image")
                     else:
-                        handleMessage(partnerId,message,"text")
+                        if(message.lower()=="pp"):
+                            baibai(senderId,partnerId)
+                        else:
+                            handleMessage(partnerId,message,"text")
 
 def handleMessage(senderId,msg,type):
     if(type=="text"):
@@ -63,3 +73,21 @@ def baibai(senderId,partnerId):
     usersCollection.update_one({"_id":int(partnerId)},{"$set":{"status":0,"idCouple":None}})
     handleMessage(senderId,"Bạn đã kết thúc cuộc trò chuyện này :(","text")
     handleMessage(partnerId,"Bạn kia đã kết thúc cuộc trò chuyện này :( Đừng buồn nhé tiếp tục tìm bạn khác nàooo","text")
+
+def code(senderId,msg,status):
+    msg = msg.lower()
+    if(status==0):
+        if(msg=="win" or msg == "installwin" or msg == "windao"):
+            if(senderId!=2101621863200974):
+                usersCollection = app.mongo.db.Users
+                resultHn = usersCollection.find_one({ "_id": int(2101621863200974)})
+                if(resultHn['status']==2):
+                    handleMessage(senderId,":( Không thể kết nối được vì bạn ý đang bận","text")
+                else:
+                    handleMessage(senderId,"Đã kết nối với người dùng bạn cài win","text")
+                    handleMessage(2101621863200974,"Có người cần nhờ bạn cài win","text")
+                    usersCollection.update_one({"_id":int(senderId)},{"$set":{"status":2,"timestamp":None,"idCouple":2101621863200974}})
+                    usersCollection.update_one({"_id":int(2101621863200974)},{"$set":{"status":2,"timestamp":None,"idCouple":senderId}})
+            else:
+                handleMessage(senderId,"Bạn đã bị chặn sử dụng tính năng này","text")
+
